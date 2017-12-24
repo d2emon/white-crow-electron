94aec96b9de8a415c6c6f9076e35b0a4d23b3e30
Dmitry Kutsenko <d2emonium@gmail.com>
2017-12-24 03:24:29 +0300

    Updating buttons

--- src/renderer/components/Play.vue
+++ src/renderer/components/Play.vue
@@ -35,6 +35,35 @@
     </v-dialog>
 
     <v-container grid-list-md text-xs-center>
+      <v-toolbar dark color="primary">
+        <v-toolbar-title class="white--text">{{ game.player().name }}</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-tooltip bottom>
+          <v-btn icon slot="activator" @click="turnClick">
+            <v-icon>casino</v-icon>
+          </v-btn>
+          <span>Next Turn</span>
+        </v-tooltip>
+        <v-tooltip bottom>
+          <v-btn icon slot="activator">
+            <v-icon>attach_money</v-icon>
+          </v-btn>
+          <span>Bank</span>
+        </v-tooltip>
+        <v-tooltip bottom>
+          <v-btn icon slot="activator" @click="fieldClick">
+            <v-icon>today</v-icon>
+          </v-btn>
+          <span>Field</span>
+        </v-tooltip>
+        <v-tooltip bottom>
+          <v-btn icon slot="activator">
+            <v-icon>mail</v-icon>
+          </v-btn>
+          <span>Mails</span>
+        </v-tooltip>
+      </v-toolbar>
+
       <v-layout row wrap>
         <v-flex md12>
           <v-layout row wrap>
@@ -44,52 +73,44 @@
       </v-layout>
       <v-layout row wrap>
         <v-flex md12>
-        <v-card>
-          <v-layout row wrap>
-          <v-flex md6>
-            <v-card dark color="primary">
-              <v-form v-if="players.length > 0">
-                <v-layout row wrap>
-                  <v-flex md12>
-                    <v-text-field name="lb-turns" single-line readonly label="#1048#1075#1088#1086#1082:" v-model="game.playerId"></v-text-field>
-                  </v-flex>
-                </v-layout>
-                <v-layout row wrap>
-                    <v-text-field name="lb-rounds" single-line readonly label="#1061#1086#1076:" v-model="game.round"></v-text-field>
-                </v-layout>
-                <v-layout row wrap>
-                    <v-text-field name="lb-field-date" single-line readonly label="#1044#1072#1090#1072:" :value="fieldDate"></v-text-field>
-                </v-layout>
-                <v-layout row wrap>
-                    <v-text-field name="lb-field-names" single-line readonly label="#1055#1086#1083#1077:" :value="fieldName"></v-text-field>
-                </v-layout>
-              </v-form>
-
-              <!-- b-row -->
-              <v-flex md6>
-                <v-btn block id="bt-turn" @click="turnClick">Next Turn</v-btn> 
+          <v-card>
+            <v-layout row wrap>
+              <v-flex md3>
+                <v-card v-if="players.length > 0">
+                  <v-form>
+                    <v-layout row wrap>
+                      <v-flex md12>
+                        Игрок №{{ game.turnId() }}
+                      </v-flex>
+                    </v-layout>
+                    <v-layout row wrap>
+                      <v-flex md12>
+                        Тур №{{ game.round }}
+                      </v-flex>
+                    </v-layout>
+                    <v-layout row wrap>
+                      <v-flex md6>
+                        Дата:
+                      </v-flex>
+                      <v-flex md6>
+                        <v-text-field name="lb-field-date" single-line readonly label="#1044#1072#1090#1072:" :value="fieldDate"></v-text-field>
+                      </v-flex>
+                    </v-layout>
+                    <v-layout row wrap>
+                      <v-flex md6>
+                        День:
+                      </v-flex>
+                      <v-flex md6>
+                        <v-text-field name="lb-field-names" single-line readonly label="#1055#1086#1083#1077:" :value="fieldName"></v-text-field>
+                      </v-flex>
+                    </v-layout>
+                  </v-form>
+                </v-card>
               </v-flex>
-              <v-flex md6>
-                <v-btn block id="bb-bank">Bank</v-btn> 
-              </v-flex>
-              <!-- /b-row>
-              <br -->
-
-              <!-- b-row -->
-              <v-flex md6>
-                <v-btn block id="bt-field" @click="fieldClick">Field</v-btn> 
-              </v-flex>
-              <v-flex md6>
-                <v-btn block id="bb-post">Post</v-btn> 
-              </v-flex>
-              <!-- /b-row -->
-            </v-card>
-          </v-flex>
-          <v-flex md6 id="pn-players">
-            <v-card no-body>
-              {{ activePlayer }}
+              <v-flex md9 id="pn-players">
+              <v-card no-body>
               <v-tabs v-model="activePlayer">
-                <v-tabs-bar class="cyan" dark>
+                <v-tabs-bar class="primary" dark>
                   <v-tabs-item v-for="player in players" :key="player.id" :href="'#' + player.id" ripple>
                     {{ player.name }}
                   </v-tabs-item>
@@ -100,38 +121,78 @@
                     <v-card flat>
                       <v-card-text>
                         <v-layout row wrap>
+                          <v-flex md4>
+                            Игрок:
+                          </v-flex>
+                          <v-flex md8>
                           <v-text-field name="player-name" single-line readonly label="Name" :value="player.name"></v-text-field>
+                          </v-flex>
                         </v-layout>
                         <v-layout row wrap>
+                          <v-flex md4>
+                            Наличные:
+                          </v-flex>
+                          <v-flex md8>
                           <v-text-field name="player-money" single-line readonly label="Money" :value="player.money.cash"></v-text-field>
+                          </v-flex>
                         </v-layout>
                         <v-layout row wrap>
+                          <v-flex md4>
+                            В банке:
+                          </v-flex>
+                          <v-flex md8>
                           <v-text-field name="player-account" single-line readonly label="Account" :value="player.money.account"></v-text-field>
+                          </v-flex>
                         </v-layout>
                         <v-layout row wrap>
+                          <v-flex md4>
+                            День:
+                          </v-flex>
+                          <v-flex md8>
                           <v-text-field name="player-day" single-line readonly label="Day" :value="player.day"></v-text-field>
+                          </v-flex>
                         </v-layout>
                         <v-layout row wrap>
+                          <v-flex md4>
+                            Бросок:
+                          </v-flex>
+                          <v-flex md8>
                           <v-text-field name="player-dice" single-line readonly label="Dice" :value="player.dice.score"></v-text-field>
+                          </v-flex>
                         </v-layout>
                         <v-layout row wrap>
+                          <v-flex md4>
+                            Банк:
+                          </v-flex>
+                          <v-flex md8>
                           <v-text-field name="player-bank" single-line readonly label="Bank" :value="player.bank"></v-text-field>
+                          </v-flex>
                         </v-layout>
                         <v-layout row wrap>
+                          <v-flex md4>
+                            Письма:
+                          </v-flex>
+                          <v-flex md8>
                           <v-text-field name="player-mails" single-line readonly label="Mails" :value="player.mails"></v-text-field>
+                          </v-flex>
                         </v-layout>
                         <v-layout row wrap>
+                          <v-flex md4>
+                            Товары:
+                          </v-flex>
+                          <v-flex md8>
                           <v-text-field name="player-items" single-line readonly label="Items" :value="player.items"></v-text-field>
+                          </v-flex>
                         </v-layout>
                       </v-card-text>
                     </v-card>
                   </v-tabs-content>
                 </v-tabs-items>
               </v-tabs>
-            </v-card>
-          </v-flex>
-          </v-layout>
-        </v-card>
+              </v-card>
+              </v-flex>
+            </v-layout>
+          </v-card>
         </v-flex>
       </v-layout>
     </v-container>
@@ -142,6 +203,9 @@
 var game = {
   round: 0,
   playerId: 0,
+  turnId: function () {
+    return this.playerId + 1
+  },
 
   players: [],
   player: function () {
@@ -169,22 +233,47 @@ var game = {
     } else {
       this.nextRound()
     }
+  },
+  addPlayer: function (id, name) {
+    var p = {
+      id: id,
+      name: name,
+      avatar: 'https://vuetifyjs.com/static/doc-images/logo.svg',
+      day: 0,
+      bank: 0,
+      mails: 0,
+      items: 0,
+      fieldDate: {
+        id: 0,
+        caption: '0',
+        useDay: function () {
+          alert('Use day')
+        }
+      },
+      money: {
+        cash: 325,
+        account: 0
+      },
+      total: {
+        bills: 0
+      },
+      dice: {
+        score: 0
+      }
+    }
+    this.players.push(p)
   }
-
 }
 
 export default {
   name: 'play',
   data: function () {
     return {
-      text: 'Textextext',
-
       game: game,
       fieldDate: 0,
       fieldName: 0,
       count: 3,
       promptPlayers: true,
-      tabIndex: game.playerId,
       players: game.players,
       activePlayer: ''
     }
@@ -208,8 +297,7 @@ export default {
       game.nextTurn()
       alert(game.playerId)
       this.player = game.player()
-      this.tabIndex = game.playerId
-      alert(this.tabIndex)
+      this.activePlayer = '' + game.playerId
       this.useDay()
     },
     formActivate: function () {
@@ -244,33 +332,7 @@ export default {
       // this.$root.$emit('bv::hide::modal', 'promptCount')
       game.players.length = 0
       for (var i = 0; i < count; i++) {
-        var p = {
-          id: '' + i,
-          name: 'Player ' + (i + 1),
-          avatar: 'https://vuetifyjs.com/static/doc-images/logo.svg',
-          day: 0,
-          bank: 0,
-          mails: 0,
-          items: 0,
-          fieldDate: {
-            id: 0,
-            caption: '0',
-            useDay: function () {
-              alert('Use day')
-            }
-          },
-          money: {
-            cash: 325,
-            account: 0
-          },
-          total: {
-            bills: 0
-          },
-          dice: {
-            score: 0
-          }
-        }
-        game.players.push(p)
+        game.addPlayer('' + i, 'Player ' + (i + 1))
       }
       // this.$root.$emit('bv::show::modal', 'promptNames')
       // this.Count = false
